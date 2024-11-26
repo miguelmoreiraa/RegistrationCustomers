99a86994049a242e52eb4be92d11e5c00f68b1c3
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2024-11-26 17:15:31 -0300

    Created Front End

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,122 @@
+import { useState, useEffect, useRef, FormEvent } from "react";
+import { FiTrash } from "react-icons/fi";
+import { api } from "./services/api";
+
+interface CustomersProps {
+  id: string;
+  name: string;
+  email: string;
+  status: boolean;
+  created_at: string;
+  updated_at: string;
+}
+function App() {
+  const [customers, setCustomers] = useState<CustomersProps[]>([]);
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+
+  async function loadCustomers() {
+    const response = await api.get("/customers");
+    setCustomers(response.data);
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    if (!nameRef.current?.value || !emailRef.current?.value) {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    const response = await api.post("/customer", {
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+    });
+
+    setCustomers((allCustomers) => [...allCustomers, response.data]);
+
+    nameRef.current.value = "";
+    emailRef.current.value = "";
+  };
+
+  const handleDelete = async (id: string) => {
+    try {
+      await api.delete("/customer", {
+        params: { id: id },
+      });
+      const allCustomers = customers.filter((customer) => customer.id !== id);
+      setCustomers(allCustomers);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadCustomers();
+  }, []);
+
+  return (
+    <>
+      <div className="w-full min-h-screen bg-gray-900 flex justify-center px-4">
+        <main className="my-10 w-full md:max-w-2xl">
+          <h1 className="text-4xl font-medium text-white">Clientes</h1>
+          <form className="flex flex-col my-6" onSubmit={handleSubmit}>
+            <label className="font-medium text-white">Nome:</label>
+            <input
+              type="text"
+              placeholder="Digite seu nome completo..."
+              className="w-full mb-5 p-2 rounded"
+              ref={nameRef}
+            />
+            <label className="font-medium text-white">E-mail:</label>
+            <input
+              type="email"
+              placeholder="Digite seu e-mail..."
+              className="w-full mb-5 p-2 rounded"
+              ref={emailRef}
+            />
+
+            <input
+              type="submit"
+              value={"Cadastrar"}
+              className="cursor-pointer w-full p-2 bg-green-500 rounded font-medium"
+            />
+          </form>
+
+          <section className="flex flex-col gap-4">
+            {customers.map((customer) => {
+              return (
+                <>
+                  <article
+                    key={customer.id}
+                    className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200"
+                  >
+                    <p>
+                      <span className="font-medium">Nome:</span> {customer.name}
+                    </p>
+                    <p>
+                      <span className="font-medium">E-mail:</span>{" "}
+                      {customer.email}
+                    </p>
+                    <p>
+                      <span className="font-medium">Status:</span>{" "}
+                      {customer.status && "ATIVO"}
+                    </p>
+
+                    <button
+                      onClick={() => handleDelete(customer.id)}
+                      className="bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute right-0 -top-2"
+                    >
+                      <FiTrash size={18} color="#fff" />
+                    </button>
+                  </article>
+                </>
+              );
+            })}
+          </section>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
